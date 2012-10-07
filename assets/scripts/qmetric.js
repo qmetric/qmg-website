7773ca5bf91dfe4d1b769667b7fe6c993a1a4cc1
Simon Smith <simon@interlopers.net>
2012-10-07 01:53:36 +0100

    Google map and orientation fix - Add google map only for tablet and higher - Orientation fix for iOS - Address bar fix loaded as module - Other CSS goodness

--- assets/scripts/qmetric.js
+++ assets/scripts/qmetric.js
@@ -3,7 +3,8 @@
         paths: {
             'mootools': 'lib/mootools',
             'mootools-more': 'lib/mootools-more',
-            'domReady': 'lib/domReady'
+            'domReady': 'lib/domReady',
+            'async': 'lib/async'
         },
         shim: {
             'mootools': {
@@ -19,7 +20,27 @@
         urlArgs: "bust=" +  (new Date()).getTime()
     });
 
-    require(['domReady', 'modules/TermsLoader', 'modules/MqShowHide'], function(domReady, TermsLoader, MqShowHide) {
+    // Find out current media query from CSS ftw
+    define('mq', function() {
+        return function() {
+            return window.getComputedStyle(document.body,':after').getPropertyValue('content');
+        }
+    });
+
+    // Create Google Maps module - http://blog.millermedeiros.com/requirejs-2-0-delayed-module-evaluation-and-google-maps/
+    define('gmaps', ['async!http://maps.googleapis.com/maps/api/js?key=AIzaSyC4a-g88iFrG8-uar7IrboJAPPvNOonGuw&sensor=true'], function() {
+        return window.google.maps;
+    });
+
+    // Loading modules for the site begins here!
+    require([
+        'domReady',
+        'modules/TermsLoader',
+        'modules/MqShowHide',
+        'mq',
+        'lib/address-bar',
+        'lib/orientation-fix'
+    ], function(domReady, TermsLoader, MqShowHide, mq, addressBar, orientationFix) {
 
         domReady(function() {
 
@@ -27,6 +48,9 @@
             new TermsLoader('.footer-links', '.hdr-terms-main > a');
 
             document.getElement('.date-year').set('text', new Date().getFullYear());
+
+            addressBar();
+            orientationFix();
 
             new MqShowHide('#nav', '.primary-nav', {
                 trigger: {
@@ -45,6 +69,27 @@
                     }
                 }
             }).hide();
+
+            // Load google maps if above phone
+            if (mq().indexOf('tablet-small') == -1 && mq().indexOf('phone') == -1) {
+                require(['gmaps'], function(gmaps) {
+
+                    var latlng = new gmaps.LatLng(51.51442, -0.07771);
+                    var mapElement = document.getElementById('google-map');
+
+                    var map = new gmaps.Map(mapElement, {
+                        zoom: 16,
+                        center: latlng,
+                        mapTypeId: gmaps.MapTypeId.ROADMAP
+                    });
+
+                    new gmaps.Marker({
+                  	    position: latlng,
+                  	    map: map
+                    });
+
+                });
+            }
 
         });
 
