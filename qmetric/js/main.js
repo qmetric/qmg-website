2c8367cb74a748e6fbda05fe38c6c0baa9c7978a
Dom Farr <dfarr@qmetric.co.uk>
2017-10-09 12:54:03 +0100

    Added staging into contact us form location

--- qmetric/js/main.js
+++ qmetric/js/main.js
@@ -1,147 +1,150 @@
+(function($) {
+    // nav - scroll to section on click
+    $('#qm-navbar a').click(function() {
+        var target = $(this).attr('data-target');
 
-(function ($) {
-	// nav - scroll to section on click
-	$('#qm-navbar a').click( function () {
-		var target = $(this).attr('data-target');
-		
-		// close nav in mobile
-		$('#qm-navbar').removeClass('in');
+        // close nav in mobile
+        $('#qm-navbar').removeClass('in');
 
-    $("html, body").animate({scrollTop: $('.'+target).offset().top - 60 }, 2000);
-	})
-
-	// forms
-  $("#formContact").submit(function(e) {
-
-    e.preventDefault();
-
-    // getting all input elemets in array
-    var inputs = $('#formContact :input');
-    // getting all values of elemets in array
-    var values = {};
-    inputs.each(function() {
-        values[this.name] = $(this).val();
-    });
-    // console.log(values);
-
-    // validate phone number field
-    if (isNaN(values['phone'])) {values.phone = "";}
-
-    $.each(values, function (key, value) {
-      var inVal = ".l-c-"+key;
-      var err = '#'+key+'Fe';
-
-      if (value == "") {
-        $(inVal).addClass('v-fail');
-        $(err).removeClass('hide');
-        if (key == "message") { 
-          $(".col-sm-6 textarea").addClass('v-fail');
-          $(err).removeClass('hide');
-        }
-      }
-      else {
-        $(inVal).removeClass('v-fail');
-        $(err).addClass('hide');
-
-        if (key == "message") { 
-          $(".col-sm-6 textarea").removeClass('v-fail');
-          $(err).addClass('hide');
-        }
-      }
+        $("html, body").animate({
+            scrollTop: $('.' + target).offset().top - 60
+        }, 2000);
     })
 
-    if($('.v-fail').length === 0) {
-	$('contact-us-form-btn').text("Sending....");
-	    
-      var URL = 'hhttps://8w7pv6tfyj.execute-api.eu-west-1.amazonaws.com/LIVE/contact-us?destination=qmetric-website';
-       var data = {
-         name: $('#name').val(),
-         email: $('#email').val(),
-         phone: $('#phone').val(),
-         message: $('#message').val()
-       };
-       
-       $.ajax({
-         type: 'POST',
-         url: URL,
-         dataType: 'json',
-         contentType: 'application/json',
-         data: JSON.stringify(data),
-         success: function () {
-           clearContactUs();
-	   $('contact-us-form-btn').text("Submit");				 
-           $('#myModal').modal('show');
-         },
-         error: function () {
-           $('contact-us-form-btn').text("Submit");
-           // show an error message
-           alert('Unfortunately, we are unable to process your request at this time.'); 
-         }
-       });
+    // forms
+    $("#formContact").submit(function(e) {
+
+        e.preventDefault();
+
+        // getting all input elemets in array
+        var inputs = $('#formContact :input');
+        // getting all values of elemets in array
+        var values = {};
+        inputs.each(function() {
+            values[this.name] = $(this).val();
+        });
+        // console.log(values);
+
+        // validate phone number field
+        if (isNaN(values['phone'])) {
+            values.phone = "";
+        }
+
+        $.each(values, function(key, value) {
+            var inVal = ".l-c-" + key;
+            var err = '#' + key + 'Fe';
+
+            if (value == "") {
+                $(inVal).addClass('v-fail');
+                $(err).removeClass('hide');
+                if (key == "message") {
+                    $(".col-sm-6 textarea").addClass('v-fail');
+                    $(err).removeClass('hide');
+                }
+            } else {
+                $(inVal).removeClass('v-fail');
+                $(err).addClass('hide');
+
+                if (key == "message") {
+                    $(".col-sm-6 textarea").removeClass('v-fail');
+                    $(err).addClass('hide');
+                }
+            }
+        })
+
+        if ($('.v-fail').length === 0) {
+
+            var stage = /www/.test(window.location) ? "LIVE" : "DEV"
+            var URL = 'https://8w7pv6tfyj.execute-api.eu-west-1.amazonaws.com/' + stage + '/contact-us'
+            var data = {
+                name: $('#name').val(),
+                email: $('#email').val(),
+                phone: $('#phone').val(),
+                message: $('#message').val()
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: URL,
+                dataType: 'json',
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                success: function() {
+                    clearContactUs();
+                    $('contact-us-form-btn').text("Submit");
+                    $('#myModal').modal('show');
+                },
+                error: function() {
+                    $('contact-us-form-btn').text("Submit");
+                    // show an error message
+                    alert('Unfortunately, we are unable to process your request at this time.');
+                }
+            });
+        }
+    });
+
+    function clearContactUs() {
+        $('#name').val("");
+        $('#email').val("");
+        $('#phone').val("");
+        $('#message').val("");
     }
-  });
-
-  function clearContactUs() {
-    $('#name').val("");
-    $('#email').val("");
-    $('#phone').val("");
-    $('#message').val("");
-  }
 
 
-  //label animation
-  function activeLabel(that) {
-    //if input empty
-    if ($(that).val() == "") {
-      var target = $(that).attr('data-target');
-      target = $('#'+target);
+    //label animation
+    function activeLabel(that) {
+        //if input empty
+        if ($(that).val() == "") {
+            var target = $(that).attr('data-target');
+            target = $('#' + target);
 
-      $(target).addClass('lab-active');
-      $(target).parent().addClass('lab-cont-active');
+            $(target).addClass('lab-active');
+            $(target).parent().addClass('lab-cont-active');
+        }
+
     }
 
-  }
-  function deactiveLabel(that) {
-    if ($(that).val() == "") {
-      var target = $(that).attr('data-target');
-      target = $('#'+target);
+    function deactiveLabel(that) {
+        if ($(that).val() == "") {
+            var target = $(that).attr('data-target');
+            target = $('#' + target);
 
-      $(target).removeClass('lab-active');
-      $(target).parent().removeClass('lab-cont-active');
+            $(target).removeClass('lab-active');
+            $(target).parent().removeClass('lab-cont-active');
+        }
     }
-  }
 
-  $('.input-field').on('autocompletechange change', function () {
+    $('.input-field').on('autocompletechange change', function() {
 
-      if ($(this).val() != "") {
-        var target = $(this).attr('data-target');
-        target = $('#'+target);
+        if ($(this).val() != "") {
+            var target = $(this).attr('data-target');
+            target = $('#' + target);
 
-        $(target).addClass('lab-active');
-        $(target).parent().addClass('lab-cont-active');
-      }
+            $(target).addClass('lab-active');
+            $(target).parent().addClass('lab-cont-active');
+        }
 
     }).change();
 
-  $('.input-field').click(function () {
-    var that = this;
-    activeLabel(that);
-  })
-  $('.input-field').focus(function () {
-    var that = this;
-    activeLabel(that);
-  })
-  $('.input-field').focusout(function () {
-    var that = this;
-    deactiveLabel(that);
-  })
+    $('.input-field').click(function() {
+        var that = this;
+        activeLabel(that);
+    })
+    $('.input-field').focus(function() {
+        var that = this;
+        activeLabel(that);
+    })
+    $('.input-field').focusout(function() {
+        var that = this;
+        deactiveLabel(that);
+    })
 
-// header animation
-$( document ).ready(function() {
+    // header animation
+    $(document).ready(function() {
 
-  $('.img2.item img').addClass('imgScale');
+        $('.img2.item img').addClass('imgScale');
 
-  $('.h-1').addClass('h-1-ani');
-});
+        $('.h-1').addClass('h-1-ani');
+    });
 
 }(jQuery));
